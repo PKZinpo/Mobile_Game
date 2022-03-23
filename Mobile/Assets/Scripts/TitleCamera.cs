b5ec3856b00bd379579c53ed91e3ef118f68e86c
PKZinpo <mbquach@gmail.com>
2022-03-23 12:41:59 -0500

    Camera changes

--- Mobile/Assets/Scripts/TitleCamera.cs
+++ Mobile/Assets/Scripts/TitleCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraRotator : MonoBehaviour {
+public class TitleCamera : MonoBehaviour {
 
     [SerializeField] private float rotateSpeed;
 
