77486fa0df53fe0b0ef1eca504b49ef3d21f2faa
PKZinpo <mbquach@gmail.com>
2022-03-23 12:04:53 -0500

    Scriptable Object fix

--- Mobile/Assets/Scripts/ScriptableObjects/Spawnable.cs
+++ Mobile/Assets/Scripts/ScriptableObjects/Spawnable.cs
@@ -2,10 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 [CreateAssetMenu(fileName = "Spawnable", menuName = "SpawnableObject", order = 1)]
 public class Spawnable : ScriptableObject {
 
@@ -14,29 +10,7 @@ public class Spawnable : ScriptableObject {
     public int parentSpawnIndexMin;
     public int parentSpawnIndexMax;
 
-    [HideInInspector] public bool randomizeGravityOn;
-    [HideInInspector] public bool gravityOn;
-    [HideInInspector] public bool randomizeMovement;
+    public bool randomizeGravityOn;
+    public bool gravityOn;
+    public bool randomizeMovement;
 }
-
-#if UNITY_EDITOR
-[CustomEditor(typeof(Spawnable))]
-public class Spawnable_Editor : Editor {
-
-    public override void OnInspectorGUI() {
-
-        DrawDefaultInspector();
-
-        Spawnable script = (Spawnable)target;
-
-        script.randomizeGravityOn = EditorGUILayout.Toggle("Random Gravity", script.randomizeGravityOn);
-
-        if (!script.randomizeGravityOn) {
-            script.gravityOn = EditorGUILayout.Toggle("Gravity On", script.gravityOn);
-            script.randomizeMovement = EditorGUILayout.Toggle("Randomize Movement", script.randomizeMovement);
-        }
-    }
-}
-
-
-#endif
